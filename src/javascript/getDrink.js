31fd2ca9cccd7277d3120ed485ad5acef25f17ba
ThikaHok <hok.thika@gmail.com>
2023-11-12 21:51:56 +0700

    Added new CSS styles and images, deleted unnecessary files, and updated API calls.

---
+++ src/javascript/getDrink.js
@@ -0,0 +1,29 @@
+function getAllDrink() {
+  $.getJSON(
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=",
+    function (data) {
+      const drinkList = $("#meal-list");
+      $.each(data.drinks, function (i, drink) {
+        const row = $("<tr>").click(function () {
+          window.location.href = "drink-details.html";
+        });
+        const imgCell = $("<td>");
+        const img = $("<img>").attr({
+          src: drink.strDrinkThumb,
+          alt: drink.strDrink,
+        });
+        imgCell.append(img);
+        row.append(imgCell);
+        const nameCell = $("<td>").text(drink.strDrink);
+        row.append(nameCell);
+        const categoryCell = $("<td>").text(drink.strCategory);
+        row.append(categoryCell);
+        const areaCell = $("<td>").text("N/A");
+        row.append(areaCell);
+        drinkList.append(row);
+      });
+    }
+  );
+}
+
+getAllDrink();
--- src/javascript/getMeal.js
+++ src/javascript/getMeal.js
@@ -1,31 +1,29 @@
 function getAllMeal() {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=")
-    .then((response) => response.json())
-    .then((data) => {
-      const mealList = document.getElementById("meal-list");
-      data.meals.forEach((meal) => {
-        const row = document.createElement("tr");
-        row.onclick = function () {
+  $.getJSON(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=",
+    function (data) {
+      const mealList = $("#meal-list");
+      $.each(data.meals, function (i, meal) {
+        const row = $("<tr>").click(function () {
           window.location.href = "food-details.html";
-        };
-        const imgCell = document.createElement("td");
-        const img = document.createElement("img");
-        img.src = meal.strMealThumb;
-        img.alt = meal.strMeal;
-        imgCell.appendChild(img);
-        row.appendChild(imgCell);
-        const nameCell = document.createElement("td");
-        nameCell.textContent = meal.strMeal;
-        row.appendChild(nameCell);
-        const categoryCell = document.createElement("td");
-        categoryCell.textContent = meal.strCategory;
-        row.appendChild(categoryCell);
-        const areaCell = document.createElement("td");
-        areaCell.textContent = meal.strArea;
-        row.appendChild(areaCell);
-        mealList.appendChild(row);
+        });
+        const imgCell = $("<td>");
+        const img = $("<img>").attr({
+          src: meal.strMealThumb,
+          alt: meal.strMeal,
+        });
+        imgCell.append(img);
+        row.append(imgCell);
+        const nameCell = $("<td>").text(meal.strMeal);
+        row.append(nameCell);
+        const categoryCell = $("<td>").text(meal.strCategory);
+        row.append(categoryCell);
+        const areaCell = $("<td>").text(meal.strArea);
+        row.append(areaCell);
+        mealList.append(row);
       });
-    });
+    }
+  );
 }
 
 getAllMeal();
