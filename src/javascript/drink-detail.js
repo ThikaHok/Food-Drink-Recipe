b7c5cd73f2d1e19c59c3c059e4b3457ffff77a2b
Sok Heang, LIM <95400680+sokheang-lim@users.noreply.github.com>
2023-11-13 13:41:48 +0700

    Add files via upload

---
+++ src/javascript/drink-detail.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  const drink = JSON.parse(sessionStorage.getItem("selectedDrink"));
+
+  $("h3").text(drink.strDrink);
+  $(".drink-image img").attr("src", drink.strDrinkThumb);
+  $(".drink-category span").text(drink.strCategory);
+  $(".drink-alcoholic span").text(drink.strAlcoholic);
+  $(".drink-glass span").text(drink.strGlass);
+  $(".food-instruction span").text(drink.strInstructions);
+
+  const ingredients = [];
+  for (let i = 1; i <= 100; i++) {
+    if (drink[`strIngredient${i}`]) {
+      ingredients.push({
+        name: drink[`strIngredient${i}`],
+        measure: drink[`strMeasure${i}`],
+      });
+    }
+  }
+
+  const tbody = $("tbody");
+  ingredients.forEach((ingredient) => {
+    const tr = $("<tr>");
+    const tdImg = $("<td>");
+    const img = $("<img>").attr(
+      "src",
+      `https://www.thecocktaildb.com/images/ingredients/${ingredient.name}.png`
+    );
+    tdImg.append(img);
+    const tdName = $("<td>").text(ingredient.name);
+    tr.append(tdImg, tdName);
+    tbody.append(tr);
+  });
+});
---
+++ src/javascript/food-detail.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  const meal = JSON.parse(sessionStorage.getItem("selectedMeal"));
+
+  $("h3").text(meal.strMeal);
+  $(".food-image img").attr("src", meal.strMealThumb);
+  $(".food-category span").text(meal.strCategory);
+  $(".food-area span").text(meal.strArea);
+  $(".food-instruction span").text(meal.strInstructions);
+
+  const ingredients = [];
+  for (let i = 1; i <= 100; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push({
+        name: meal[`strIngredient${i}`],
+        measure: meal[`strMeasure${i}`],
+      });
+    }
+  }
+
+  const tbody = $("tbody");
+  ingredients.forEach((ingredient) => {
+    const tr = $("<tr>");
+    const tdImg = $("<td>");
+    const img = $("<img>").attr(
+      "src",
+      `https://www.themealdb.com/images/ingredients/${ingredient.name}.png`
+    );
+    tdImg.append(img);
+    const tdName = $("<td>").text(ingredient.name);
+    tr.append(tdImg, tdName);
+    tbody.append(tr);
+  });
+});
